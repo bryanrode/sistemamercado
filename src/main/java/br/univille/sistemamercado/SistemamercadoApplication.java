37cca35f26ac1c89971a696dc04f4700a1152ed9
BryanRode <bryanrode2005@gmail.com>
2022-11-16 09:30:25 -0300

    first commit

---
+++ src/main/java/br/univille/sistemamercado/SistemamercadoApplication.java
@@ -0,0 +1,13 @@
+package br.univille.sistemamercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemamercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemamercadoApplication.class, args);
+	}
+
+}
